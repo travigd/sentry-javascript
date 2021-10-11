0ff703b56759e646c4117238c9806d00ee07ba96
Onur Temizkan <onur@narval.co.uk>
2021-10-11 21:14:48 +0000

    Add `getSentryEvents`

--- packages/browser/test/e2e/test/utils/helpers.js
+++ packages/browser/test/e2e/test/utils/helpers.js
@@ -1,3 +1,5 @@
+const Sentry = require('@sentry/browser');
+
 function waitForXHR(xhr, cb) {
   if (xhr.readyState === 4) {
     return cb();
@@ -8,24 +10,52 @@ function waitForXHR(xhr, cb) {
   }, 1000 / 60);
 }
 
-async function runInSandbox(page, fn, globals = {}) {
+async function setGlobals(page, globals = {}) {
   for (const [globalName, globalVal] of Object.entries(globals)) {
     // Only need to pass a function for the PoC, so, we'll probably need to improve this to support other global types.
     await page.addScriptTag({ content: `var ${globalName} = ${globalVal};` });
   }
+}
 
+async function runInSandbox(page, fn, globals = {}) {
+  await setGlobals(page, globals);
   await page.addScriptTag({ content: `(${fn})();` });
 }
 
-async function getSentryEvent(page, fn, globals) {
+async function getSentryRequest(page, fn, globals) {
   const request = (
     await Promise.all([runInSandbox(page, fn, globals), page.waitForRequest(/.*.sentry\.io\/api.*/gm)])
   )[1];
+
   return JSON.parse(request.postData());
+}
+
+async function getSentryEvents(page, fn, globals = {}) {
+  await setGlobals(page, globals);
+
+  const eventsHandle = (
+    await Promise.all([
+      page.evaluateHandle(() => {
+        const events = [];
+
+        const scope = Sentry.getCurrentHub().getScope();
+
+        scope.addEventProcessor(function(event) {
+          events.push(event);
+          return event;
+        });
+        return events;
+      }),
+      page.addScriptTag({ content: `(${fn})();` }),
+    ])
+  )[0];
+
+  return eventsHandle.jsonValue();
 }
 
 module.exports = {
   waitForXHR,
   runInSandbox,
-  getSentryEvent,
+  getSentryRequest,
+  getSentryEvents,
 };
