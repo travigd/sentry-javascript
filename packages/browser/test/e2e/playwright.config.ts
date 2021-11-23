ed908d0e29f14fce44e363b1244b0106e5252932
Onur Temizkan <onur@narval.co.uk>
2021-11-23 13:19:50 +0000

    Update Playwright config.

--- packages/browser/test/e2e/playwright.config.ts
+++ packages/browser/test/e2e/playwright.config.ts
@@ -2,7 +2,6 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   retries: 2,
-  timeout: 12000,
-  workers: 3,
+  timeout: 5000,
 };
 export default config;
