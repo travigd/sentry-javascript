e1dba4ba9da9a274684f3643944937bab0e7c381
Onur Temizkan <onur@narval.co.uk>
2021-12-01 10:52:13 +0000

    Document helpers.

--- packages/browser/test/e2e/utils/browserHelpers.ts
+++ packages/browser/test/e2e/utils/browserHelpers.ts
@@ -1,3 +1,10 @@
+/**
+ * Helper to wait for an XHR to finish
+ *
+ * @param {{ readyState: number }} xhr
+ * @param {() => unknown} cb
+ * @returns {*}  {unknown}
+ */
 export function waitForXHR(xhr: { readyState: number }, cb: () => unknown): unknown {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     return cb();
